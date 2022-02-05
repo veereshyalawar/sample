80fb216fc1c4215ae9b0656d01b160729b5c0baf
veeresh <veereshyalawar2@gmail.com>
2022-02-05 04:09:35 -0800

    added uc8 file

---
+++ uc-8.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+maxHoursInMonth=100
+empRatePerHr=20
+numOfWorkingDays=20
+totalEmpHr=0
+totalWorkingDays=0
+ 
+function getWorkingHours()
+{
+        case $1 in
+                        $isFullTime )
+                                empHrs=8                                
+                                ;;
+                        $isPartTime )
+                                empHrs=4
+                                ;;
+                        *)
+                                empHrs=0
+                                ;;
+        esac
+}
+
+function getEmpWage()
+{
+   echo $(($1*$empRatePerHr))
+}
+while [[ $totalEmpHr -lt $maxHoursInMonth &&
+       $totalWorkingDays -lt $numOfWorkingDays ]]
+do
+   ((totalWorkingDays++))
+   randomCheck=$((RANDOM%3))
+   getWorkingHours $randomCheck
+   totalEmpHr=$(($totalEmpHr+$empHrs))
+   dailyWages[$totalWorkingDays]=$(($empHrs*$empRatePerHr))
+done
+
+totalSalary="$( getEmpWage $totalEmpHr )"
+echo ${dailyWages[@]}
+
